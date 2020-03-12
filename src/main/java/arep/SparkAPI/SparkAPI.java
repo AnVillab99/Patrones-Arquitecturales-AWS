6105d84fb5ca1c380f15442c3aca0654b80759e8
AnVillab99 <34347348+AnVillab99@users.noreply.github.com>
2020-03-12 16:07:48 -0500

    lol

--- src/main/java/arep/SparkAPI/SparkAPI.java
+++ src/main/java/arep/SparkAPI/SparkAPI.java
@@ -18,8 +18,29 @@ public class SparkAPI {
         staticFiles.location("/static");
         Gson gson = new Gson();
         port(getPort());
-        System.out.println("ya?");
+        options("/*",
+        (request, response) -> {
 
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        System.out.println("ya?");
+        
         post("/registro", (req, res) -> {
             System.out.println("entrooooooooo");
             
